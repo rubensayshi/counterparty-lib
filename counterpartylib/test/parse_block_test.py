b22edcc523caa51a06858c3d95e922cd552ba2a6
Ruben de Vries <ruben@rubensayshi.com>
2016-06-21 13:15:54 +0200

    parse block test fixtures

--- counterpartylib/test/parse_block_test.py
+++ counterpartylib/test/parse_block_test.py
@@ -14,10 +14,11 @@ FIXTURE_DB = tempfile.gettempdir() + '/fixtures.parseblock_unittest_fixture.db'
 
 def test_parse_block(server_db):
     test_outputs = blocks.parse_block(server_db, DP['default_block_index'], 1420914478)
-    outputs = ('baa1c5b432094ebc0d0d817db8e874e112d2cc539632a6754bce699e5fb1643f',
-               '72a73f5a73384114b1acb451e20ac98716ee44cb37b03404d341a4bfef8d0efc',
-               '0da70d5c8da450373de695385677d9428374cd3ee2715f39792664e1c5c73f58',
+    outputs = ('97f3cd418615653ee514300f1c7a2f52f42564ed45d664d1b7420ee4bf54aaf1',
+               'a6748035a8fef3cbffc65c4856d4d11e60dfa213d6cc6795ef384b47be55817c',
+               'bcb6b454e353d41de95112268eb504d2ab72bc052598fa274dc4af17a660bc1f',
                None)
+
 
     try:
         assert outputs == test_outputs
